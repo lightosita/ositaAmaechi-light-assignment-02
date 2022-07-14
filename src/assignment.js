69415ba1a0f18c77b9860b9913483fe7f6c3d919
Light Ihechi Osita-Amaechi <lightazuh@yahoo.com>
2022-07-14 19:05:26 +0000

    completed-Altschool-Assignment02

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,15 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum = 0;
+    for (let i = 0; i < arrayOfNumbers.length; i++) {
+        sum = sum + arrayOfNumbers[i]; 
+    };
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+    
+
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -27,10 +33,20 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
-function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+function
+ countEvenNumbers(arrayOfNumbers) {
+    let evenNumbers = 0;
+    for(let i = 0; i < arrayOfNumbers.length; i++){
+
+        if(arrayOfNumbers[i] % 2 
+            === 0) {
+
+            evenNumbers += 1;
+        }
+    }
+    return evenNumbers;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
